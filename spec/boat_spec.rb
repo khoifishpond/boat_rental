7e9b766a38addb116f429758d99c7d4e0ba1c6d9
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-11 02:32:33 -0700

    Add Attributes to spec

--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -4,7 +4,17 @@ require './lib/boat'
 describe Boat do
   kayak = Boat.new(:kayak, 20)
 
-  it 'exists' do
-    expect(kayak).to be_a(Boat)
+  context 'Attributes' do
+    it 'exists' do
+      expect(kayak).to be_a(Boat)
+    end
+
+    it 'has a type' do
+      expect(kayak.type).to eq(:kayak)
+    end
+
+    it 'has a price per hour' do
+      expect(kayak.price_per_hour).to eq(20)
+    end
   end
 end
