bddd8fcb3d8d171e7231f3f1902abf3c970e2e21
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-11 22:16:45 -0700

    Add charge method to class

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,13 +1,24 @@
 class Dock
-  attr_reader :name, :max_rental_time, :rental_log
+  attr_reader :name, :max_rental_time, :rental_log, :charged
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
     @rental_log = {}
+    @charged = {}
   end
 
   def rent(boat, renter)
     @rental_log[boat] = renter
+  end
+
+  def charge(boat)
+    if boat.hours_rented > @max_rental_time
+      boat.hours_rented = @max_rental_time
+    end
+
+    @charged[:card_number] = @rental_log[boat].credit_card_number
+    @charged[:amount] = boat.hours_rented * boat.price_per_hour
+    @charged
   end
 end
